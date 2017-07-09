a96793217a2354be4302d0f08df31b27cf2c1957
buzatina <tlntlatleng@gmail.com>
2017-07-10 01:52:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ passport.use(new LocalStrategy(
    , assert = require('assert');
 
   // Connection URL
-  var url = process.env.MONGOURI;
+  var url = 'mongodb://tebogo:mokgaga19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
   // Use connect method to connect to the Server
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
@@ -101,7 +101,7 @@ passport.deserializeUser(function(id, done){
    , assert = require('assert');
 
   // Connection URL
-  var url = process.env.MONGOURI;
+  var url = 'mongodb://tebogo:mokgaga19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
   // Use connect method to connect to the Server
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
@@ -281,7 +281,7 @@ app.get('/listbiz/list', function(req, res){
                                , assert = require('assert');
 
                               // Connection URL
-                              var url = process.env.MONGOURI;
+                              var url = 'mongodb://tebogo:mokgaga19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
                               // Use connect method to connect to the Server
                               MongoClient.connect(url, function(err, db){
                                 assert.equal(null, err);
