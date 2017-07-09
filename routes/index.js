ba97e27e4549d8d88113e9c88685efdacf994251
buzatina <tlntlatleng@gmail.com>
2017-07-10 01:46:05 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.post('/', function(req, res){
 				 , assert = require('assert');
 
 				// Connection URL
-				var url = process.env.MONGOURI;
+				var url = 'mongodb://tebogo:mokgaga19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
 				// Use connect method to connect to the Server
 				MongoClient.connect(url, function(err, db){
 				  assert.equal(null, err);
@@ -55,7 +55,7 @@ router.get('/category/:category', function(req,res){
 				 , assert = require('assert');
 
 				// Connection URL
-				var url = process.env.MONGOURI;
+				var url = 'mongodb://tebogo:mokgaga19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
 				// Use connect method to connect to the Server
 				MongoClient.connect(url, function(err, db) {
 				  assert.equal(null, err);
@@ -95,7 +95,7 @@ router.get('/biz/:bizid', function(req, res){
 				 , assert = require('assert');
 
 				// Connection URL
-				var url = process.env.MONGOURI;
+				var url = 'mongodb://tebogo:mokgaga19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
 				// Use connect method to connect to the Server
 				MongoClient.connect(url, function(err, db) {
 				  assert.equal(null, err);
@@ -149,7 +149,7 @@ router.post('/rate', function(req, res){
 	   , assert = require('assert');
 
 	  // Connection URL
-	  var url = process.env.MONGOURI;
+	  var url = 'mongodb://tebogo:mokgaga19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
 	  // Use connect method to connect to the Server
 	  MongoClient.connect(url, function(err, db){
 	    assert.equal(null, err);
