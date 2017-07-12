850689a23ba885fbf3788fcb19e8d33214213aca
buzatina <tlntlatleng@gmail.com>
2017-07-13 00:54:51 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // create http server
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-server.listen(process.env.PORT || 2716);
+server.listen(process.env.PORT || 3000);
 
 // Set the View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -44,7 +44,7 @@ passport.use(new LocalStrategy(
    , assert = require('assert');
 
   // Connection URL
-  var url = 'mongodb://tebogo:mokgaga19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
+  var url = 'mongodb://max:max19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
   // Use connect method to connect to the Server
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
@@ -94,14 +94,14 @@ passport.serializeUser(function(user, done) {
   done(null, user.username);
 });
 
-passport.deserializeUser(function(id, done){
+passport.deserializeUser(function(id, done) {
  
     // Connect To a Database
   var MongoClient = require('mongodb').MongoClient
    , assert = require('assert');
 
   // Connection URL
-  var url = 'mongodb://tebogo:mokgaga19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
+  var url = 'mongodb://max:max19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
   // Use connect method to connect to the Server
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
@@ -125,13 +125,10 @@ passport.deserializeUser(function(id, done){
 
           });
 
-
-    // End insert single document
-
     });
 
 });
-
+ 
 // Express Session
 app.use(session({
 	secret: 'secret',
@@ -181,8 +178,20 @@ app.use(function(req, res, next){
 
 app.post('/users/login', 
   passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/listbiz/list');
+  function(req, res){
+    res.redirect('/company/profile');
+});
+
+app.get('/users/login', function(req, res){
+
+  if (req.user) {
+
+    res.redirect('/company/profile');
+
+  };
+
+  res.render('login');
+
 });
 
 app.get('/users/logout', function(req, res){
@@ -197,44 +206,252 @@ app.use('/users', users);
 // What Is the best way 
 var router = express.Router();
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 2709));
 
-//List A business
-app.get('/listbiz/list', function(req, res){
+//profile Pic Event
+app.get('/company/profile', function(req, res){
+
+    if (req.user) {} else{res.redirect('/users/login')};
+
+
+    console.log('It is detecting something');
     
     var sendUser = req.user;
 
     sendUser.password = '';
 
-    res.render('addlisting', {user: sendUser});
+    var nsp = io.of('/'+req.user._id);
 
+      // Connect To a Database
+    var MongoClient = require('mongodb').MongoClient
+     , assert = require('assert');
 
-      var nsp = io.of('/'+req.user._id);
+    // Connection URL
+    var url = 'mongodb://max:max19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+
+        // Rate Business
+      db.collection('sites').findOne({userid: ''+req.user._id}, function(err, site){
+        
+              if (err) {
+ 
+                res.render('companynew', {user: sendUser});
+
+              } else {
+
+                console.log(site);
+                console.log(req.user._id);
+                
+                res.render('companynew', {user: sendUser, site: site});
+
+              };
+
+            });
+
+      });
+      
       nsp.on('connection', function(socket){
 
           console.log('One socket connected');
 
-          var newListingBoolean = true;
+          var receiveDataPic = true;
+          var receiveDataVideo = true;
+          var receiveDataAbout = true;
+          var receiveDataContact = true;
+          var receiveDataSocial = true;
 
-          socket.on('newListing', function(dataBoss){
+          socket.on('socialProfile', function(dataBoss){
 
-            newListingBoolean = false;
+              if (receiveDataSocial) {
 
-            list(dataBoss);
-            
+                receiveDataSocial = false;
+
+                setProfileSocial(dataBoss);
+                
+              };           
+
           });
 
+          socket.on('contactProfile', function(dataBoss){
+
+              if (receiveDataContact) {
+
+                receiveDataContact = false;
+
+                setProfileContact(dataBoss);
+                
+              };           
+
+          });
+
+          socket.on('aboutProfile', function(dataBoss){
+
+              if (receiveDataAbout) {
+
+                receiveDataAbout = false;
+
+                setProfileAbout(dataBoss);
+                
+              };           
+
+          });
+
+          socket.on('newProfilePic', function(dataBoss){
+
+              if (receiveDataPic) {
+
+                receiveDataPic = false;
+
+                setProfilePic(dataBoss);
+                
+              };           
+
+          });
+
+          socket.on('newProfileVideo', function(dataBoss){
+
+              if (receiveDataVideo) {
+
+                receiveDataVideo = false;
+
+                setProfileVideo(dataBoss);
+                
+              };           
+
+          });
+
+        /// Set Social Info
+            var setProfileSocial = function(data){
+            
+                  // Connect To a Database
+                var MongoClient = require('mongodb').MongoClient
+                 , assert = require('assert');
+
+                // Connection URL
+                var url = 'mongodb://max:max19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
+                // Use connect method to connect to the Server
+                MongoClient.connect(url, function(err, db) {
+                  assert.equal(null, err);
+
+                    // Rate Business
+                  db.collection('sites').update({userid: data.userid}, {facebook: data.facebook, twitter: data.twitter, linkedin: data.linkedin, instagram: data.instagram}, {upsert: true}, function(err, result){
+                          
+                          if (err) {
+
+                            console.log(err);
+
+                            receiveDataSocial = true;
+
+                            socket.emit('errUploading', 'everyone');
+
+                          } else {
+                            
+                            console.log('Updated Max');
+
+                            socket.emit('uploadedSocialEvent', 'everyone');
+
+                            receiveDataSocial = true;
+
+                          };
+
+
+                        });                  
+                  });
+        }; 
+
+        /// Set Contact Info
+            var setProfileContact = function(data){
+            
+                  // Connect To a Database
+                var MongoClient = require('mongodb').MongoClient
+                 , assert = require('assert');
+
+                // Connection URL
+                var url = 'mongodb://max:max19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
+                // Use connect method to connect to the Server
+                MongoClient.connect(url, function(err, db) {
+                  assert.equal(null, err);
+
+                    // Rate Business
+                  db.collection('sites').update({userid: data.userid}, {$set:{userid: data.userid, address: data.address, phone: data.phone, contactEmail: data.contactEmail}}, {upsert: true}, function(err, result){
+                          if (err) {
+
+                            console.log(err);
+
+                            receiveDataContact = true;
+
+                            socket.emit('errUploadingContact', 'everyone');
+
+                          } else {
+                            
+                            console.log('Updated Max');
+
+                            socket.emit('uploadedContactEvent', 'everyone');
+
+                            receiveDataContact = true;
+
+                          };
+
+                        });
+
+                  });
+
+        };          
+
+        /// Set About and Page Title
+            var setProfileAbout = function(data){
+            
+                  // Connect To a Database
+                var MongoClient = require('mongodb').MongoClient
+                 , assert = require('assert');
+
+                // Connection URL
+                var url = 'mongodb://max:max19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
+                // Use connect method to connect to the Server
+                MongoClient.connect(url, function(err, db) {
+                  assert.equal(null, err);
+
+                    // Rate Business
+                  db.collection('sites').update({userid: data.userid}, {$set:{userid: data.userid, about: data.about, title: data.title}}, {upsert: true}, function(err, result){
+                          if (err) {
+
+                            console.log(err);
+
+                            receiveDataAbout = true;
+
+                            socket.emit('errAboutUploading', 'everyone');
+
+                          } else {
+                            
+                            console.log('Updated Max');
+
+                            socket.emit('uploadedAboutEvent', 'everyone');
+
+                            receiveDataAbout = true;
+
+                          };
+
+                        });
+
+                  });
+
+        };          
 
         /// UPLOAD FILE METHOD START
-            var list = function(data){
+            var setProfileVideo = function(data){
+
+            console.log('Uploading Video');
+            console.log(data);
             
             var file = data.file;
 
             var AWS = require('aws-sdk');
             AWS.config = new AWS.Config();
 
-            AWS.config.accessKeyId = 'AKIAJTAHEHCE7ITIOAMA';
-            AWS.config.secretAccessKey = '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';
+            AWS.config.accessKeyId = 'AKIAJXQLIK4D4DIUET6Q';
+            AWS.config.secretAccessKey = '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';
 
             //  Get userid from front side
                var url;
@@ -248,12 +465,15 @@ app.get('/listbiz/list', function(req, res){
 
                       Bucket: bucketName
                     }
-
                   });
 
                 if (file) {
 
-                    var objKey = 'Lacat/' + data.userid+ data.fileName;
+                    //Object key will be facebook-USERID#/FILE_NAME
+
+                    // var objKey = 'Lacat/' + data.userId;
+
+                    var objKey = 'buzatinalive/' + data.userid+ data.fileName;
 
                     var urlPic = 'https://s3-us-west-2.amazonaws.com/buzatina/'+objKey;
 
@@ -268,39 +488,56 @@ app.get('/listbiz/list', function(req, res){
                     };
 
                     bucket.putObject(params, function (err, dataObject) {
-                        if (err){
+                        if (err) {
                             
-                            console.log('Error uploading pic');
+                            socket.emit('errVideoUploading', 'everyone');
+
+                            receiveDataVideo = true;
 
                             console.log(err);
 
                         } else {
 
-                              // Connect To a Database
+
+                                // Connect To a Database
                               var MongoClient = require('mongodb').MongoClient
                                , assert = require('assert');
 
                               // Connection URL
-                              var url = 'mongodb://tebogo:mokgaga19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
+                              var url = 'mongodb://max:max19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
                               // Use connect method to connect to the Server
-                              MongoClient.connect(url, function(err, db){
+                              MongoClient.connect(url, function(err, db) {
                                 assert.equal(null, err);
-       
-                                  // Add event to the database
-                                db.collection('businesses').insertOne({userid: data.userid, companyName: data.companyName, contactEmail: data.contactEmail, phone: data.phone, loxion: data.loxion, address: data.address, fileType: data.fileType, about: data.about, category: data.category, fileUrl: urlPic, fileKey: objKey}, function(err, result){
+
+                                  // Rate Business
+                                db.collection('sites').update({userid: data.userid}, {$set: {userid: data.userid, userid: data.userid, videoProfile: true, videofileType: data.actualFileType, videofileUrl: urlPic, videofileKey: objKey}}, {upsert: true}, function(err, result){
                                         if (err) {
 
                                           console.log(err);
 
+                                          receiveDataVideo = true;
+
+                                          socket.emit('errVideoUploading', 'everyone');
+
                                         } else {
                                           
-                                          socket.emit('Listed', 'everyone');
+                                          console.log('Updated Max');
+
+                                          socket.emit('uploadedVideoEvent', 'everyone');
+
+                                          receiveDataVideo = true;
+
 
                                         };
 
                                       });
 
+                                // End insert single document
+
                                 });
+
+
+                            console.log('File Uploaded');
 
                         }
 
@@ -308,7 +545,113 @@ app.get('/listbiz/list', function(req, res){
 
                 } else {
 
-                    console.log('No file to upload');
+                    socket.emit('errVideoUploading', 'everyone');
+
+                    receiveDataVideo = true;
+
+                };
+        };
+
+        /// UPLOAD FILE METHOD START
+            var setProfilePic = function(data){
+            
+            var file = data.file;
+
+            var AWS = require('aws-sdk');
+            AWS.config = new AWS.Config();
+
+            AWS.config.accessKeyId = 'AKIAJXQLIK4D4DIUET6Q';
+            AWS.config.secretAccessKey = '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';
+
+            //  Get userid from front side
+               var url;
+         
+               //Upload to S3
+               AWS.config.region = 'us-west-2';
+               var bucketName = 'buzatina';
+               var bucket = new AWS.S3({
+
+                 params: {
+
+                      Bucket: bucketName
+                    }
+                  });
+
+                if (file) {
+
+                    //Object key will be facebook-USERID#/FILE_NAME
+
+                    // var objKey = 'Lacat/' + data.userId;
+
+                    var objKey = 'buzatinalive/' + data.userid+ data.fileName;
+
+                    var urlPic = 'https://s3-us-west-2.amazonaws.com/buzatina/'+objKey;
+
+                    var params = {
+
+                        Key: objKey,
+
+                        ContentType: data.actualFileType,
+
+                        Body: file
+                        
+                    };
+
+                    bucket.putObject(params, function (err, dataObject) {
+                        if (err) {
+                            
+                            socket.emit('errPictureUploading', 'everyone');
+
+                            console.log(err);
+
+                        } else {
+
+
+                                // Connect To a Database
+                              var MongoClient = require('mongodb').MongoClient
+                               , assert = require('assert');
+
+                              // Connection URL
+                              var url = 'mongodb://max:max19916@ds155920-a0.mlab.com:55920,ds155920-a1.mlab.com:55920/buzatinalive?replicaSet=rs-ds155920';
+                              // Use connect method to connect to the Server
+                              MongoClient.connect(url, function(err, db) {
+                                assert.equal(null, err);
+
+                                  // Rate Business
+                                db.collection('sites').update({userid: data.userid}, {$set: {userid: data.userid, picProfile: true, picfileType: data.fileType, picfileUrl: urlPic, picfileKey: objKey}}, {upsert: true}, function(err, result){
+                                        if (err) {
+
+                                          console.log(err);
+                                          receiveDataPic = true;
+                                          socket.emit('errPictureUploading', 'everyone');
+
+                                        } else {
+                                          
+                                          console.log('Updated Max');
+
+                                          receiveDataPic = true;
+
+                                          socket.emit('uploadedPictureEvent', 'everyone');
+
+
+                                        };
+
+                                      });
+
+                                // End insert single document
+
+                                });
+
+                          console.log('File Uploaded');
+
+                        }
+
+                    });
+
+                } else {
+
+                    socket.emit('errPictureUploading', 'everyone');
+                    receiveDataPic = true;
 
                 };
         };
